216401ac9d201e901adb63ea7f9c27fca21d647a
Genta Wibowo <genta.wibowo@bukalapak.com>
2022-12-29 09:51:52 +0700

    43:18

--- code/player.py
+++ code/player.py
@@ -7,10 +7,11 @@ class Player(pygame.sprite.Sprite):
         super().__init__(group)
 
         self.import_assets()
+        self.status = 'down_idle'
+        self.frame_index = 0
 
         # geenral setup
-        self.image = pygame.Surface((32,64))
-        self.image.fill('green')
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center = pos)
 
         # movement attritube
@@ -28,6 +29,7 @@ class Player(pygame.sprite.Sprite):
         for animation in self.animations.keys():
             full_path = '../graphics/character/' + animation
             self.animations[animation] = import_folder(full_path)
+        print(self.animations)
 
     def input(self):
         keys = pygame.key.get_pressed()
